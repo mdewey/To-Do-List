0d3ada7f05c359c656f690352027593ab4f0ab57
unknown <Mark@Auron.(none)>
2012-09-03 23:27:41 -0400

    Add item to list, delete item from list

--- src/me/dewey/todo/ToDoListActivity.java
+++ src/me/dewey/todo/ToDoListActivity.java
@@ -1,13 +1,142 @@
 package me.dewey.todo;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ToDoListActivity extends Activity {
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ToDoListActivity extends ListActivity {
+	
+	//TODO: Add database support for data living past one life
+	//private GroupDataSource data source;
+	private List<String> listItems;
+    private ArrayAdapter<String> adapter;
+    private ListView myList;
+    private Vibrator vibe;
+    final Context context = this;
+	    
+	
+	/** Android Events **/
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //gets list of items and inits the listitem array
+        listItems = GetListOfItems();
+        
+        //inits vibrator for long button click
+        vibe = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE) ;
+        
+        //sets adapter for listview
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listItems);
+        setListAdapter(adapter);
+        
+        //set onclick listners 
+        myList = (ListView)findViewById(android.R.id.list);
+        myList.setOnItemClickListener(new OnItemClickListener() {
+	
+			public void onItemClick(AdapterView<?> arg0, View view, int position,
+					long id) {
+					//TODO figure out what do to when an item is clicked....
+					Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+        myList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			public boolean onItemLongClick(AdapterView<?> arg0, View view,final int pos, long id) {
+				vibe.vibrate(50);
+				AlertDialog.Builder builder = new AlertDialog.Builder(ToDoListActivity.this);
+				builder.setMessage("Are you sure you want to delete " +listItems.get(pos));
+				builder.setCancelable(false);
+				builder.setPositiveButton("Yes", new OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						RemoveItemFromList(pos);
+
+					}
+				});
+				builder.setNegativeButton("No", new OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+							//Nothing happends here
+					}
+				});
+				AlertDialog alert = builder.create();
+				alert.show();
+				return true;
+			}
+		});
+        
     }
+    /** helper Functions **/
+     
+    private ArrayList<String> GetListOfItems()
+    {
+    	//TODO: Query Database and get the list from datasource
+    	return new ArrayList<String>();
+		
+    }
+    
+    public void ShowAddItemDialog(View v)
+    {
+      LayoutInflater li = LayoutInflater.from(context);
+      View display = li.inflate(R.layout.get_item_dialog_layout, null);
+      
+      AlertDialog.Builder adb = new AlertDialog.Builder(context);
+      
+      adb.setView(display);
+      final EditText userInput = (EditText)display.findViewById(R.id.itemTitle);
+      
+      adb.setCancelable(true)
+         .setPositiveButton("Add!", new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(context, userInput.getText(), Toast.LENGTH_SHORT).show();
+				String itemToAdd = userInput.getText().toString();
+				//TODO: Insert into DB
+	    		//Group newGroup = datasource.createGroup(textToAdd);
+	    		//add value to list
+	    		adapter.add(itemToAdd);
+	        	adapter.notifyDataSetChanged();
+				
+			}
+		})
+		.setNegativeButton("Cancel!", new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				dialog.cancel();
+			}
+		});
+      
+      AlertDialog alertDialog = adb.create();
+      
+      alertDialog.show();
+ 
+    }
+    
+    public void RemoveItemFromList(int pos)
+    {
+    	//TODO: Delete from datasource once datasource is added......
+    	adapter.remove(adapter.getItem(pos));
+    	adapter.notifyDataSetChanged();
+    }
+
 }
